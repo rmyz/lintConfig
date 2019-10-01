e62aeecd37a4c5069ce5ab2c47763f05771324fb
Sergi Romeu <sergi.romeu@tiendeo.com>
2019-10-01 16:40:43 +0200

    fix lintConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,39 @@
 const prettierOptions = require('./.prettierrc.js');
-const {CUSTOM_RULES, LEVEL_RULES} = require('./constants');
+const { CUSTOM_RULES, LEVEL_RULES } = require('./constants');
 
 module.exports = {
-    "env": {
-        "browser": true,
-        "cypress/globals": true,
-        "es6": true,
-        "jest": true,
-        "node": true
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react', 'prettier/standard'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "prettier",
-        "prettier",
-        "prettier/react",
-        "prettier/standard"
-    ],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['babel', 'cypress', 'prettier', 'react', 'react-hooks', 'import'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [LEVEL_RULES.ERROR, prettierOptions],
+    ...CUSTOM_RULES,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './config/dist.js',
+      },
     },
-    "plugins": [
-        "babel",
-        "cypress",
-        "prettier",
-        "react",
-        "react-hooks"
-    ],
-    "rules": {
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        'prettier/prettier': [LEVEL_RULES.ERROR, prettierOptions],
-        ...CUSTOM_RULES
-    }
+    'import/core-modules': ['config'],
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 const CUSTOM_RULES = require('./customRules');
-const LEVEL_RULES = requires('./levelRules');
+const LEVEL_RULES = require('./levelRules');
 
-export {CUSTOM_RULES, LEVEL_RULES};
+module.exports = { CUSTOM_RULES, LEVEL_RULES };
